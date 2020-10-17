ed910c4b58105f82eecdf4e41187227b7d005d97
MadfishDT <lafefish@gmail.com>
2020-10-17 17:00:28 +0900

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '925131ee16ec7edc19dd6a56c684b363'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '25db330e-2f46-4e50-831f-6a71f11e36ad'
+};
